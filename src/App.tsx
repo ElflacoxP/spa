79e7c588bedd6453fe2124cb9ed212399e81cb35
ElflacoxP <leorodoni25@gmail.com>
2023-07-15 17:03:37 -0300

    todo lo demas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import './styles.css'
+import IMAGE from './react.png'
+import LOGO from './logo.svg'
+import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-    return <h1>React typeScript Webpack starter Template</h1>
+  return (
+    <>
+      <h1>
+        Edited Text Hello React typeScript Webpack starter Template -{' '}
+        {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React logo" width="300" height="200" />
+      <img src={LOGO} alt="React SVG" width="300" />
+      <ClickCounter />
+    </>
+  )
 }
